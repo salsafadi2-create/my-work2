e06c2c024776ec96616057f327536f65e18fbb7c
salsafadi2-create <salsafadi2@smail.ucas.edu.ps>
2026-01-07 16:00:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+    // const modal = document.getElementById("workModal");
+    // const modalImg = document.getElementById("modalTargetImg");
+    // const closeBtn = document.querySelector(".close-modal-btn");
+
+    // // تشغيل النافذة عند الضغط على أي صورة تملك كلاس clickable-img
+    // document.querySelectorAll('.clickable-img').forEach(img => {
+    //     img.addEventListener('click', function() {
+    //         modal.style.display = "flex";
+    //         modalImg.src = this.src;
+    //     });
+    // });
+
+    // // إغلاق النافذة عند الضغط على زر X
+    // closeBtn.onclick = () => {
+    //     modal.style.display = "none";
+    // };
+
+    // // إغلاق النافذة عند الضغط في أي مكان خارج المحتوى الأبيض
+    // window.onclick = (event) => {
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //     }
+    // };
